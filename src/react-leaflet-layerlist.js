e596ee6e84a9dd4d72ccc7e70bc485191fc31c1b
Alpal94 <terminator@DESKTOP-65KR3ML.localdomain>
2019-06-18 14:28:22 +0800

    Fixed bug in compare function

--- src/react-leaflet-layerlist.js
+++ src/react-leaflet-layerlist.js
@@ -172,11 +172,12 @@ L.Control.LayerListControl = L.Control.extend({
 		var cache = [];
 		var stringify = JSON.stringify(node, function(key, value) {
 			if (typeof value === 'object' && value !== null) {
-				if (cache.indexOf(value) !== -1 || key.includes("Time") || key.includes("time")) {
+				if (cache.indexOf(value) !== -1) {
 					return;
 				}
 				cache.push(value);
 			}
+			if(key === "_owner") return undefined;
 			return value;
 		});
 		cache = null; 
